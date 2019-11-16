b9df2a17f0d3f0567d9cbc0f1dc064d3dda7d3aa
Jryke <rykerson@gmail.com>
2019-11-16 16:47:02 -0500

    typo fixed and exported directly

--- database.js
+++ database.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
 
-let database = mongoose.connect(process.env.MONGODB_URI, {
-  userNewURLParser: true,
+module.exports = mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true
 }, (err) => {
   err ? console.log(err) : console.log('Connected to MongoDB')
 })
-
-module.exports = database
