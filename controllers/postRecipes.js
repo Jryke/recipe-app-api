b9773112fba17b0cd602ab3cecffcf4a1f66a201
Jryke <rykerson@gmail.com>
2019-11-16 17:51:07 -0500

    error log changed to res.send

--- controllers/postRecipes.js
+++ controllers/postRecipes.js
@@ -3,5 +3,5 @@ const Recipes = require('../models/recipes.js')
 module.exports = (req, res) => {
   Recipes.create(req.body)
     .then(data => res.send(data))
-    .catch(err => console.log(err))
+    .catch(err => res.send(err))
 }
